0afaa1d59942e3b8f8fc72e012d8007d16698ae4
serjm1 <70753509+serjm1@users.noreply.github.com>
2020-09-04 13:57:34 +0300

    practice1

---
+++ lesson1_ikbo1919/src/ru/mirea/Main.java
@@ -0,0 +1,10 @@
+package ru.mirea;
+public class main {
+
+    public static void main(String []args) {
+   /* Это первая моя java-программа.
+   В результате выполнения на экран будет выведено 'Привет мир!'
+   */
+        System.out.println("Привет мир!"); // Вывод сообщения на экран
+    }
+}
